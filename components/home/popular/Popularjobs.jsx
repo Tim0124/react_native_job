fc75f06a6ea728f6f48b1d9adb236c7a9ac168d1
Tim Kao <a0925172969@gmail.com>
2023-11-23 19:06:39 +0800

    feat: add popularjob UI component

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -1,12 +1,50 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import { useState } from 'react'
+import { View, Text, TouchableOpacity, FlatList, ActivityIndicator } from 'react-native'
+import { useRouter } from 'expo-router'
 
 import styles from './popularjobs.style'
+import { COLORS, SIZES } from '../../../constants'
+import PoplarJobCard from '../../common/cards/popular/PopularJobCard.jsx'
+import useFetch from '../../../hook/useFetch'
 
 const Popularjobs = () => {
+  const router = useRouter()
+  const { data, isLoading, error } = useFetch('search',{
+    query: 'React developer',
+    num_pages: '1',
+  })
+  const [selectedJob, setSelectJob] = useState()
+
+  const handleCardPress = () => {
+    
+  }
+  
+
   return (
-    <View>
-      <Text>Popularjobs</Text>
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.headerTitle}>Popular jobs</Text>
+        <TouchableOpacity>
+          <Text style={styles.headerBtn}>Show all</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.container}>
+        {isLoading ? (
+          <ActivityIndicator size="large" color={COLORS.primary}/>
+        ) : error ? (
+          <Text>Something went wrong</Text>
+        ) : (
+          <FlatList
+            data={data}
+            renderItem={({item}) => (
+              <PoplarJobCard item={item}/>
+            )}
+            keyExtractor={item => item?.job_id}
+            contentContainerStyle={{ columnGap: SIZES.medium}}
+            horizontal
+          />
+        )}
+      </View>
     </View>
   )
 }
