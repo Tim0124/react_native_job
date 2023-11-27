2b63a8f4152c98ec621586d0490d995ab016d6ad
Tim Kao <a0925172969@gmail.com>
2023-11-27 23:21:39 +0800

    feat: add responsibilities components

--- app/job-details/[id].js
+++ app/job-details/[id].js
@@ -25,6 +25,7 @@ const JobDetails = () => {
           case "About":
             return <JobAbout info={data[0].job_description ?? 'No data provided'}/>
             case "Responsibilities":
+              return <Specifics title="Responsibilities" points={data[0].job_highlights?.Responsibilities ?? ['N/A']} />
               default:
                 break
     }
